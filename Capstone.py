96fb53715011fd7070bdd76c9631a19aec8bc4b9
Anjali Bhardwaj <134281230+anjaliJMI@users.noreply.github.com>
2024-01-06 22:46:19 +0530

    Add files via upload

---
+++ Capstone.py
@@ -0,0 +1,289 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Requests allows us to make HTTP requests which we will use to get data from an API
+import requests
+# Pandas is a software library written for the Python programming language for data manipulation and analysis.
+import pandas as pd
+# NumPy is a library for the Python programming language, adding support for large, multi-dimensional arrays and matrices, along with a large collection of high-level mathematical functions to operate on these arrays
+import numpy as np
+# Datetime is a library that allows us to represent dates
+import datetime
+
+# Setting this option will print all collumns of a dataframe
+pd.set_option('display.max_columns', None)
+# Setting this option will print all of the data in a feature
+pd.set_option('display.max_colwidth', None)
+
+
+# ## Defining a series of helper functions
+# 
+
+# In[3]:
+
+
+# Takes the dataset and uses the rocket column to call the API and append the data to the list
+def getBoosterVersion(data):
+    for x in data['rocket']:
+        response = requests.get("https://api.spacexdata.com/v4/rockets/"+str(x)).json()
+        BoosterVersion.append(response['name'])
+
+
+# In[4]:
+
+
+# Takes the dataset and uses the launchpad column to call the API and append the data to the list
+def getLaunchSite(data):
+    for x in data['launchpad']:
+        response = requests.get("https://api.spacexdata.com/v4/launchpads/"+str(x)).json()
+        Longitude.append(response['longitude'])
+        Latitude.append(response['latitude'])
+        LaunchSite.append(response['name'])
+
+
+# In[5]:
+
+
+# Takes the dataset and uses the payloads column to call the API and append the data to the lists
+def getPayloadData(data):
+    for load in data['payloads']:
+        response = requests.get("https://api.spacexdata.com/v4/payloads/"+load).json()
+        PayloadMass.append(response['mass_kg'])
+        Orbit.append(response['orbit'])
+
+
+# In[6]:
+
+
+# Takes the dataset and uses the cores column to call the API and append the data to the lists
+def getCoreData(data):
+    for core in data['cores']:
+            if core['core'] != None:
+                response = requests.get("https://api.spacexdata.com/v4/cores/"+core['core']).json()
+                Block.append(response['block'])
+                ReusedCount.append(response['reuse_count'])
+                Serial.append(response['serial'])
+            else:
+                Block.append(None)
+                ReusedCount.append(None)
+                Serial.append(None)
+            Outcome.append(str(core['landing_success'])+' '+str(core['landing_type']))
+            Flights.append(core['flight'])
+            GridFins.append(core['gridfins'])
+            Reused.append(core['reused'])
+            Legs.append(core['legs'])
+            LandingPad.append(core['landpad'])
+
+
+# In[7]:
+
+
+spacex_url="https://api.spacexdata.com/v4/launches/past"
+
+
+# In[8]:
+
+
+response = requests.get(spacex_url)
+
+
+# In[9]:
+
+
+import requests
+
+spacex_url = "https://api.spacexdata.com/v4/launches/latest"
+response = requests.get(spacex_url)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Parse and use the content of the response
+    spacex_data = response.json()
+    # Do something with spacex_data
+else:
+    # Handle the error, print an error message, or raise an exception
+    print(f"Error: {response.status_code}")
+
+
+# In[10]:
+
+
+static_json_url='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/API_call_spacex_api.json'
+
+
+# In[11]:
+
+
+response.status_code
+
+
+# In[12]:
+
+
+pip install requests
+
+
+# In[14]:
+
+
+200.status_code
+
+
+# In[15]:
+
+
+# Use json_normalize meethod to convert the json result into a dataframe
+data=pd.json_normalize(response.json())
+
+
+# In[16]:
+
+
+# Displaying first 5 rows
+data.head()
+
+
+# In[17]:
+
+
+# Lets take a subset of our dataframe keeping only the features we want and the flight number, and date_utc.
+data = data[['rocket', 'payloads', 'launchpad', 'cores', 'flight_number', 'date_utc']]
+
+# We will remove rows with multiple cores because those are falcon rockets with 2 extra rocket boosters and rows that have multiple payloads in a single rocket.
+data = data[data['cores'].map(len)==1]
+data = data[data['payloads'].map(len)==1]
+
+# Since payloads and cores are lists of size 1 we will also extract the single value in the list and replace the feature.
+data['cores'] = data['cores'].map(lambda x : x[0])
+data['payloads'] = data['payloads'].map(lambda x : x[0])
+
+# We also want to convert the date_utc to a datetime datatype and then extracting the date leaving the time
+data['date'] = pd.to_datetime(data['date_utc']).dt.date
+
+# Using the date we will restrict the dates of the launches
+data = data[data['date'] <= datetime.date(2020, 11, 13)]
+data.head()
+
+
+# In[18]:
+
+
+#Global variables 
+BoosterVersion = []
+PayloadMass = []
+Orbit = []
+LaunchSite = []
+Outcome = []
+Flights = []
+GridFins = []
+Reused = []
+Legs = []
+LandingPad = []
+Block = []
+ReusedCount = []
+Serial = []
+Longitude = []
+Latitude = []
+
+
+# In[19]:
+
+
+BoosterVersion
+
+
+# In[20]:
+
+
+# Call getBoosterVersion
+getBoosterVersion(data)
+
+
+# In[21]:
+
+
+BoosterVersion[0:5]
+
+
+# In[22]:
+
+
+# Call getLaunchSite
+getLaunchSite(data)
+
+# Call getPayloadData
+getPayloadData(data)
+
+# Call getCoreData
+getCoreData(data)
+
+
+# In[23]:
+
+
+launch_dict = {'FlightNumber': list(data['flight_number']),
+'Date': list(data['date']),
+'BoosterVersion':BoosterVersion,
+'PayloadMass':PayloadMass,
+'Orbit':Orbit,
+'LaunchSite':LaunchSite,
+'Outcome':Outcome,
+'Flights':Flights,
+'GridFins':GridFins,
+'Reused':Reused,
+'Legs':Legs,
+'LandingPad':LandingPad,
+'Block':Block,
+'ReusedCount':ReusedCount,
+'Serial':Serial,
+'Longitude': Longitude,
+'Latitude': Latitude}
+
+
+# In[24]:
+
+
+# Matplotlib is a plotting library for python and pyplot gives us a MatLab like plotting framework. We will use this in our plotter function to plot data.
+import matplotlib.pyplot as plt
+#Seaborn is a Python data visualization library based on matplotlib. It provides a high-level interface for drawing attractive and informative statistical graphics
+import seaborn as sns
+
+
+# In[25]:
+
+
+sns.catplot(y="PayloadMass", x="FlightNumber", hue="Class", data=df, aspect = 5)
+plt.xlabel("Flight Number",fontsize=20)
+plt.ylabel("Pay load Mass (kg)",fontsize=20)
+plt.show()
+
+
+# In[26]:
+
+
+# Plotting a scatter point chart with x axis to be Flight Number and y axis to be the launch site, and hue to be the class value
+sns.catplot(y='LaunchSite', x='FlightNumber', hue='Class', data=df, aspect=5)
+plt.xlabel("Flight Number",fontsize=20)
+plt.ylabel("LaunchSite",fontsize=20)
+plt.show()
+
+
+# In[27]:
+
+
+# A function to Extract years from the date 
+year=[]
+def Extract_year(df):
+    for i in df["Date"]:
+        year.append(i.split("-")[0])
+    return year
+
+
+# In[ ]:
+
+
+
+
